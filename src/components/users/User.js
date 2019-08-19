294472dce19f2d8622ba4feb3f36f91efdfd5cdc
wisimple <arifsamisahin@gmail.com>
2019-08-19 23:17:56 +0300

    User.js class based component is changed to functional component and useEffect is implemented

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import CustomSpinner from '../layout/CustomSpinner';
@@ -15,117 +15,115 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 library.add(faChevronLeft);
 
-class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
+const User = ({ user, getUser, getUserRepos, repos, loading, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
 
-  static propTypes = {
-    user: PropTypes.object.isRequired,
-    loading: PropTypes.bool,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired
-  };
-
-  render() {
-    const {
-      login,
-      avatar_url,
-      bio,
-      hireable,
-      name,
-      public_repos,
-      email,
-      company,
-      blog,
-      html_url
-    } = this.props.user;
-    const { loading, repos } = this.props;
-    if (loading) return <CustomSpinner />;
-    return (
-      <Fragment>
-        <Row>
-          <Col>
-            <Link to='/'>
-              <Button variant='light'>
-                <FontAwesomeIcon icon='chevron-left' /> Back
-              </Button>
-            </Link>
-          </Col>
-        </Row>
-        <Row className='text-center'>
-          <Col>
-            <Image src={avatar_url} style={{ width: '120px' }} rounded />
-            <br />
-            <span>{login}</span>
-          </Col>
-        </Row>
-        <Row className='text-center'>
-          <Col>
-            <h2>{name}</h2>
-            {bio && (
-              <Fragment>
-                <span>{bio}</span>
-                <br />
-              </Fragment>
-            )}
-            {email && (
-              <Fragment>
-                <strong>Email: </strong>
-                <a href={'mailto:' + email}>{email}</a>
-                <br />
-              </Fragment>
-            )}
-            {company && (
-              <Fragment>
-                <strong>Company: </strong> {company}
-                <br />
-              </Fragment>
-            )}
-            {blog && (
-              <Fragment>
-                <strong>Blog: </strong>{' '}
-                <a href={blog} target='_blank' rel='noopener noreferrer'>
-                  {blog}
-                </a>
-                <br />
-              </Fragment>
-            )}
-            <div className='my-2'>
-              <a
-                href={html_url + '?tab=repositories'}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <Badge variant='secondary'>
-                  {'Public repos: '} {public_repos}
-                </Badge>{' '}
+  const {
+    login,
+    avatar_url,
+    bio,
+    hireable,
+    name,
+    public_repos,
+    email,
+    company,
+    blog,
+    html_url
+  } = user;
+  if (loading) return <CustomSpinner />;
+  return (
+    <Fragment>
+      <Row>
+        <Col>
+          <Link to='/'>
+            <Button variant='light'>
+              <FontAwesomeIcon icon='chevron-left' /> Back
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+      <Row className='text-center'>
+        <Col>
+          <Image src={avatar_url} style={{ width: '120px' }} rounded />
+          <br />
+          <span>{login}</span>
+        </Col>
+      </Row>
+      <Row className='text-center'>
+        <Col>
+          <h2>{name}</h2>
+          {bio && (
+            <Fragment>
+              <span>{bio}</span>
+              <br />
+            </Fragment>
+          )}
+          {email && (
+            <Fragment>
+              <strong>Email: </strong>
+              <a href={'mailto:' + email}>{email}</a>
+              <br />
+            </Fragment>
+          )}
+          {company && (
+            <Fragment>
+              <strong>Company: </strong> {company}
+              <br />
+            </Fragment>
+          )}
+          {blog && (
+            <Fragment>
+              <strong>Blog: </strong>{' '}
+              <a href={blog} target='_blank' rel='noopener noreferrer'>
+                {blog}
               </a>
-              {hireable ? (
-                <Badge variant='success'>Hireable</Badge>
-              ) : (
-                <Badge variant='warning'>Not Hierable</Badge>
-              )}
-            </div>
-            <Button
-              href={html_url}
+              <br />
+            </Fragment>
+          )}
+          <div className='my-2'>
+            <a
+              href={html_url + '?tab=repositories'}
               target='_blank'
               rel='noopener noreferrer'
-              size='sm'
-              variant='secondary'
             >
-              Visit Github Profile
-            </Button>
-          </Col>
-        </Row>
-        <hr />
-        <h4>Repositories</h4>
-        <Repos repos={repos} />
-      </Fragment>
-    );
-  }
-}
+              <Badge variant='secondary'>
+                {'Public repos: '} {public_repos}
+              </Badge>{' '}
+            </a>
+            {hireable ? (
+              <Badge variant='success'>Hireable</Badge>
+            ) : (
+              <Badge variant='warning'>Not Hierable</Badge>
+            )}
+          </div>
+          <Button
+            href={html_url}
+            target='_blank'
+            rel='noopener noreferrer'
+            size='sm'
+            variant='secondary'
+          >
+            Visit Github Profile
+          </Button>
+        </Col>
+      </Row>
+      <hr />
+      <h4>Repositories</h4>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
+
+User.propTypes = {
+  user: PropTypes.object.isRequired,
+  loading: PropTypes.bool,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired
+};
 
 export default User;
