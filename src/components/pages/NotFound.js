b3970e471912e82346b976d12604fa9df4987761
wisimple <arifsamisahin@gmail.com>
2019-08-20 15:17:04 +0300

    Go To Home link added to Not found page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const NotFound = () => {
   return (
     <div>
       <h1>Not found!</h1>
       <p>The page you are looking for not found.</p>
+      <Link to='/'>
+        <Button variant='danger'>Go To Home Page</Button>
+      </Link>
     </div>
   );
 };
